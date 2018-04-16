4cce3badfba04093f0c45ffd6ff7a19605b6e910
Mikael Mathisen <mikaelma@stud.ntnu.no>
2018-04-16 14:06:46 +0200

    HEi hans

--- ProjectPartTwo.py
+++ ProjectPartTwo.py
@@ -2,3 +2,9 @@ from pyspark import SparkContext
 sc = SparkContext(appName="tweets")
 
 distFile = sc.textFile("data/geotweets.tsv")
+
+tweets = distFile.map(lambda line: line.split('\t'))
+tweetolini = tweets.map(lambda tweet: (tweet[4], tweet[10]))
+print(tweetolini.take(10))
+
+sc.stop()
